57387eb0690f188006bcb444a4e524e3e8adcfa7
trincellshelvin <trincellshelvin@gmail.com>
2024-08-20 02:35:06 +0000

    started js for form data

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+let state = {
+    firtname: "",
+    lastname: "",
+    petsname: "",
+    phonenumber: "",
+    email: "",
+    city: "",
+    zipcode: ""
+};
+
+let savbtn = document.getElementById(savebutton);
