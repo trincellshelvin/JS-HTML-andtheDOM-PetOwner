93f5dec0e3fcf5c28ba6858231bbddb3a4739a02
trincellshelvin <trincellshelvin@gmail.com>
2024-08-20 02:51:18 +0000

    stopped at creation of handleSave functon.

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ let state = {
 };
 
 let savbtn = document.getElementById(savebutton);
+let firstnameInput = document.getElementById(firstname);
+let lastnameInput = document.getElementById(lastname);
+let petsnameInput = document.getElementById(phonenumber);
+let emailInput = document.getElementById(email);
+let cityInput = document.getElementById(city);
+let zipcodeInput = document.getElementById(zipcode);
+
+function handleSave(){
+    console.log("Save button was clicked");
+    let name = firstnameInput.value;
+    state.name = name;
+    state.lastname = lastnameInput;
+    state.petsname = petsnameInput;
+    state.email = emailInput;
+    
+
+
+}
