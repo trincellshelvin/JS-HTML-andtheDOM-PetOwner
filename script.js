0ff5bf86b6728e13adcaaa1e989934b0ea2b3a63
Trincell Shelvin <trincellshelvin@gmail.com>
2024-08-21 12:01:17 -0500

    fixed form issues with dev tools errors and checked functions for eventlisteners errors where button still isn't working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("hello world");
+
 let state = {
-    firtname: "",
+    firstname: "",
     lastname: "",
     petsname: "",
     phonenumber: "",
@@ -9,38 +10,40 @@ let state = {
     zipcode: ""
 };
 
-let savebtn = document.getElementById(savebutton);
-let firstnameInput = document.getElementById(firstname);
-let lastnameInput = document.getElementById(lastname);
-let petsnameInput = document.getElementById(petsname);
-let phonenumberInput = document.getElementById(phonenumber);
-let emailInput = document.getElementById(email);
-let cityInput = document.getElementById(city);
-let zipcodeInput = document.getElementById(zipcode);
+let savebtn = document.getElementById("savebutton");
+let firstnameInput = document.getElementById("firstname");
+let lastnameInput = document.getElementById("lastname");
+let petsnameInput = document.getElementById("petsname");
+let phonenumberInput = document.getElementById("phonenumber");
+let emailInput = document.getElementById("email");
+let cityInput = document.getElementById("city");
+let zipcodeInput = document.getElementById("zipcode");
 
-function handleSave(){
+function handleSave() {
     console.log("Save button was clicked");
-    let name = firstnameInput.value;
-    state.name = name;
-    state.lastname = lastnameInput;
-    state.petsname = petsnameInput;
-    state.phonenumber = phonenumberInput;
-    state.email = emailInput;
-    state.city = cityInput;
-    state.zipcode =zipcodeInput;
-console.log("Add name" + state.name);
-console.log(state);
-firstnameInput.value = "";
-lastnameInput.value = "";
-petsnameInput.value = "";
-phonenumberInput.value = "";
-emailInput.value = "";
-cityInput.value = "";
-zipcodeInput.value = "";
+    state.firstname = firstnameInput.value;
+    state.lastname = lastnameInput.value;
+    state.petsname = petsnameInput.value;
+    state.phonenumber = phonenumberInput.value;
+    state.email = emailInput.value;
+    state.city = cityInput.value;
+    state.zipcode = zipcodeInput.value;
+
+    console.log("Add name: " + state.firstname);
+    console.log(state);
+
+    firstnameInput.value = "";
+    lastnameInput.value = "";
+    petsnameInput.value = "";
+    phonenumberInput.value = "";
+    emailInput.value = "";
+    cityInput.value = "";
+    zipcodeInput.value = "";
 }
 
-let user = petOwner(state.name, state.lastname, state.petsname, state.phonenumber, state.email, state.city, state.zipCode);
+let user = createPetOwner(state.firstname, state.lastname, state.petsname, state.phonenumber, state.email, state.city, state.zipcode);
 user.status();
 user.render();
 
-savebutton.addEventListener("click", handleSave, createPetOwner);
+savebtn.addEventListener("click", handleSave);
+savebtn.addEventListener("click", createPetOwner);
