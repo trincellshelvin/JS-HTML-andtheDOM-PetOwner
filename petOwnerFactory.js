e7e2cbb07e833b8ec1c61fab3457c48be9323811
Trincell Shelvin <trincellshelvin@gmail.com>
2024-08-21 11:47:10 -0500

    corrected render function syntax error

--- petOwnerFactory.js
+++ petOwnerFactory.js
@@ -1,6 +1,6 @@
 function createPetOwner(firstname, lastname, petsname, phonenumber, email, city, zipcode) {
     let petOwner = {
-        first: name,
+        first: firstname,
         last: lastname,
         pets: petsname,
         phone: phonenumber,
@@ -9,9 +9,9 @@ function createPetOwner(firstname, lastname, petsname, phonenumber, email, city,
         zipcode: zipcode,
         status: function () {
             console.log(`First Name: ${this.first} Last Name: ${this.last} Pet's Name: ${this.pets} Phone Number: ${this.phone} 
-                Email: ${this.email} City: ${this.city} Zip Code: ${this.zipcode}`)
-        }
-        render: function(){
+                Email: ${this.email} City: ${this.city} Zip Code: ${this.zipcode}`);
+        },
+        render: function() {
             let pOwnerFname = document.getElementById("firsNameOutput");
             pOwnerFname.textContent = this.first;
             let pOwnerLname = document.getElementById("lastNameOutput");
@@ -26,27 +26,7 @@ function createPetOwner(firstname, lastname, petsname, phonenumber, email, city,
             cIty.textContent = this.city;
             let zipCoDe = document.getElementById("zipCodeOutput");
             zipCoDe.textContent = this.zipcode;
-        },
-        return petOwner;
-    }
+        }
+    };
+    return petOwner;
 }
-
-
-//render: function() {
-   // let nameP = document.getElementById("userNameOutput");
-   // nameP.textContent = this.name;
-   // let bDayP = document.getElementById("userBirthdayOutput");
-    //bDayP.textContent = this.birthday;
-    //let zipP = document.getElementById("userZipCodeOutput");
-    //zipP.textContent = this.zipCode;
-//}
-//}
-//return person;
-//}
-
-// Proof of life
-// let p1 = createPerson("Bob", "1-1-2017", "90210");
-// console.log(p1);
-// p1.status();
-// let p2 = createPerson("Alice", "1-2-2017", "90210");
-// p2.status();
