f4fefea46abcdae50f90c9c5e8d836547e2a96f0
Trincell Shelvin <trincellshelvin@gmail.com>
2024-08-25 13:42:50 -0500

    can't get corrected state.js to render petOwner details in the console and localstoreage application from  user input

--- state.js
+++ state.js
@@ -1,40 +1,23 @@
 console.log("hi");
 let state = {
     pets: [],
-    petOwner: [],
-};
-let firstname = localStorage.getItem("firstname");
-console.log(firstname);
-let lastname = localStorage.getItem("lastname");
-console.log(lastname);
-let petsname = localStorage.getItem("petsname");
-console.log(petsname);
-let phonenumber = localStorage.getItem("phonenumber");
-console.log(phonenumber);
-let email = localStorage.getItem("email");
-console.log(email);
-let city = localStorage.getItem("city");
-console.log(city);
-let zipcode = localStorage.getItem("zipcode");
-console.log(zipcode);
+}
 
+// Retrieve data from localStorage and log it
+state.firstname = localStorage.getItem("firstname");
+//console.log(state.firstname);
+state.lastname = localStorage.getItem("lastname");
+//c//onsole.log(state.lastname);
+state.petsname = localStorage.getItem("petsname");
+//console.log(state.petsname);
+state.phonenumber = localStorage.getItem("phonenumber");
+console.log(state.phonenumber);
+state.email = localStorage.getItem("email");
+//console.log(state.email);
+state.city = localStorage.getItem("city");
+//console.log(state.city);
+state.zipcode = localStorage.getItem("zipcode");
+//console.log(state.zipcode);
 
-
-state.firstname = firstname;
-state.lastname = lastname; 
-state.petsname = petsname;
-state.phonenumber = phonenumber;
-state.email = email;
-state.city = city;
-state.zipcode = zipcode;
-
-firstnameOutput.value = "";;
-lastnameOutput.value = "";
-petsnameOutput.value = "";
-phonenumberOutput.value = "";
-emailOutput.value = "";
-cityOutput.value = "";
-zipcodeOutput.value = "";
-
-let user = createPetOwner(state.firstname, state.lastname, state.petsname, state.phonenumber, state.email, state.city, state.zipcode)
+let user = createPetOwner(state.firstname, state.lastname, state.petsname, state.phonenumber, state.email, state.city, state.zipcode);
 
