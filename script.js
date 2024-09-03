8ae50e8c5f3235993c004c9dac409f7d5ad65cb6
Trincell Shelvin <trincellshelvin@gmail.com>
2024-09-02 20:54:46 -0500

    got functions to save to local storage and console. will not render on page at all with nothing done.

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 console.log("hello world");
-    let savebtn = document.getElementById("savebutton");
-    let firstnameInput = document.getElementById("firstnameOutput");
-    let lastnameInput = document.getElementById("lastnameOutput");
-    let petsnameInput = document.getElementById("petsnameOutput");
-    let phonenumberInput = document.getElementById("phonenumberOutput");
-    let emailInput = document.getElementById("emailOutput");
-    let citynameInput = document.getElementById("citynameOutput");
-    let zipcodeInput = document.getElementById("zipcodeOutput");
 
-    function handleSave() {
-        console.log("Save button was clicked");
+let savebtn = document.getElementById("savebutton");
+let firstnameInput = document.getElementById("firstname");
+let lastnameInput = document.getElementById("lastname");
+let petsnameInput = document.getElementById("petsname");
+let phonenumberInput = document.getElementById("phonenumber");
+let emailInput = document.getElementById("email");
+let citynameInput = document.getElementById("cityname");
+let zipcodeInput = document.getElementById("zipcode");
 
-        localStorage.setItem("firstname", state.firstname);
-        localStorage.setItem("lastname", state.lastname);
-        localStorage.setItem("petsname", state.petsname);
-        localStorage.setItem("phonenumber", state.phonenumber);
-        localStorage.setItem("email", state.email);
-        localStorage.setItem("cityname", state.cityname);
-        localStorage.setItem("zipcode", state.zipcode);
+function handleSave() {
+    console.log("Save button was clicked");
 
+    let state = {
+        firstname: firstnameInput.value,
+        lastname: lastnameInput.value,
+        petsname: petsnameInput.value,
+        phonenumber: phonenumberInput.value,
+        email: emailInput.value,
+        cityname: citynameInput.value,
+        zipcode: zipcodeInput.value
+    };
 
-        
+    localStorage.setItem("firstname", state.firstname);
+    localStorage.setItem("lastname", state.lastname);
+    localStorage.setItem("petsname", state.petsname);
+    localStorage.setItem("phonenumber", state.phonenumber);
+    localStorage.setItem("email", state.email);
+    localStorage.setItem("cityname", state.cityname);
+    localStorage.setItem("zipcode", state.zipcode);
 
-        let user = createPetOwner(state.firstname, state.lastname, state.petsname, state.phonenumber, state.email, state.cityname, state.zipcode);
-        user.status();
-        user.render();
-    }
+    let user = createPetOwner(state.firstname, state.lastname, state.petsname, state.phonenumber, state.email, state.cityname, state.zipcode);
+    user.status();
+    user.render();
+}
+
+savebtn.addEventListener("click", handleSave);
+
 
 
     
@@ -39,7 +50,7 @@ function createPetOwner(firstname, lastname, petsname, phonenumber, email, cityn
         petsname: "",
         phonenumber: "",
         email: "",
-        city: "",
+        cityname: "",
         zipcode: "",
         status: function () {
             console.log(`Owner: ${this.firstname} ${this.lastname}, Pet: ${this.petsname}`);
