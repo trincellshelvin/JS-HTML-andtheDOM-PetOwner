fa15cbccef3f68295bef86eb3c0b7461ab59eddb
Trincell Shelvin <trincellshelvin@gmail.com>
2024-09-02 19:46:30 -0500

    fixed error that prevented console log of user input. still fixing render issue in html where it is not showing on the page

--- petOwnerFactory.js
+++ petOwnerFactory.js
@@ -24,8 +24,28 @@ function createPetOwner(firstname, lastname, petsname, phonenumber, email, city,
     return petOwner;
 }
 
-// Example usage
-// let p = createPetOwner("Trincell", "Shelvin", "Phoebe", "2252252222", "trincell@funny.com", "Baton Rouge", "70806");
-// console.log(p); 
-// p.status();
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    let state = {
+        firstname: document.getElementById('firstname').value,
+        lastname: document.getElementById('lastname').value,
+        petsname: document.getElementById('petsname').value,
+        phonenumber: document.getElementById('phonenumber').value,
+        email: document.getElementById('email').value,
+        cityname: document.getElementById('cityname').value,
+        zipcode: document.getElementById('zipcode').value
+    };
+
+    localStorage.setItem('firstname', state.firstname);
+    localStorage.setItem('lastname', state.lastname);
+    localStorage.setItem('petsname', state.petsname);
+    localStorage.setItem('phonenumber', state.phonenumber);
+    localStorage.setItem('email', state.email);
+    localStorage.setItem('cityname', state.cityname);
+    localStorage.setItem('zipcode', state.zipcode);
+
+    let user = createPetOwner(state.firstname, state.lastname, state.petsname, state.phonenumber, state.email, state.cityname, state.zipcode);
+
+    user.render();
+});
